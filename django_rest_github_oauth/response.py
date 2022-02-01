497e0ddb27cccdd4611ec7a56da5ec2c5368c5f9
Abdullah Adeel <easyautosol@gmail.com>
2022-02-01 23:23:35 +0500

    ✅ added tokens return in response

--- django_rest_github_oauth/response.py
+++ django_rest_github_oauth/response.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.response import Response
@@ -28,6 +29,34 @@ class APIResponse:
 
 
 class UserResponse:
+    @staticmethod
+    def generate_token(user: User):
+        INSTALLED_APPS = getattr(settings, 'INSTALLED_APPS', None)
+        if 'rest_framework.authtoken' not in INSTALLED_APPS:
+            raise Exception("You need to add 'rest_framework.authtoken' to your INSTALLED_APPS")
+
+        from rest_framework.authtoken.models import Token
+        token, _ = Token.objects.get_or_create(user=user)
+        return {"token": token.key}
+
+    @staticmethod
+    def generate_jwt_token(user: User):
+        try:
+            from rest_framework_simplejwt.tokens import RefreshToken
+        except ImportError:
+            raise ImportError("rest_framework_simplejwt needs to be installed")
+        token = RefreshToken.for_user(user)
+        return {
+            'access': str(token.access_token),
+            'refresh': str(token)
+        }
+
+    @staticmethod
+    def get_auth_token(user: User):
+        GITHUB_AUTH_USE_JWT = getattr(settings, "GITHUB_AUTH_USE_JWT", False)
+        if GITHUB_AUTH_USE_JWT:
+            return UserResponse.generate_jwt_token(user)
+        return UserResponse.generate_token(user)
 
     @staticmethod
     def get_user_payload(user: User):
@@ -35,5 +64,5 @@ class UserResponse:
             "username": user.username,
             "email": user.email,
             "id": user.id,
-            # "token": user.get_tokens()
+            **UserResponse.get_auth_token(user)
         }
